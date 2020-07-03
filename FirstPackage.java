39c2b33050c38ca43a82b6f8c5a613c55df65005
Sagar Pal <57063071+sagar510@users.noreply.github.com>
2020-07-03 14:34:00 +0530

    Add files via upload

---
+++ FirstPackage.java
@@ -0,0 +1,11 @@
+import myPack.*;
+class FirstPackage
+{
+ public static void main(String args[])
+{
+   subpgm1 obj1=new subpgm1();
+   subpgm2 obj2=new subpgm2();
+  obj1.disp();
+obj2.display();
+}
+}
