17b540dd5ced22db710bac30535e166bb795c2e6
Sagar Pal <57063071+sagar510@users.noreply.github.com>
2020-07-03 14:35:21 +0530

    Add files via upload

---
+++ subpgm1.java
@@ -0,0 +1,8 @@
+package myPack;
+public class subpgm1
+{
+ public void disp()
+{
+  System.out.println("This is sub program 1");
+}
+}
---
+++ subpgm2.java
@@ -0,0 +1,8 @@
+package myPack;
+public class subpgm2
+{
+ public void display()
+{
+ System.out.println("This is Sub Program 2");
+}
+}
